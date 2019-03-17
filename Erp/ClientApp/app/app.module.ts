53e205326d8e46a94bc3a108bf1af83c0cdf4469
Abdullah Hesham <AbdullahHesham309@gmail.com>
2019-03-17 22:22:59 +0200

    System Page

--- Erp/ClientApp/app/app.module.ts
+++ Erp/ClientApp/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ModuleList } from './services/moduleList.component';
-import { Cart } from './services/cart.component';
+//import { Cart } from './services/cart.component';
 import { DataService } from './shared/dataService';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
       AppComponent,
-      ModuleList,
-      Cart
+      ModuleList
+      //Cart
   ],
   imports: [
       BrowserModule,
