255654757bbbcccf0ea4ccd5dc888af4ee39b412
Abdullah Hesham <AbdullahHesham309@gmail.com>
2019-03-21 22:43:09 +0200

    بظبط التظابيط
    مرحبا ابو عيد

--- Erp/ClientApp/app/services/cart.component.ts
+++ Erp/ClientApp/app/services/cart.component.ts
@@ -4,10 +4,18 @@ import { DataService } from '../shared/dataService';
 @Component({
     selector: 'the-cart',
     templateUrl: 'cart.component.html',
-    styleUrls: [],
+    styleUrls: ['cart.component.css'],
 })
 
 export class Cart {
     constructor(private data: DataService) {
+    }
+
+    openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+    }
+
+    closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
     }
 }
