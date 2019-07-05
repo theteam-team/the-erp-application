24f255316f9e37b37c7f2044091f9a02257bf6d0
jemy <mohamedgamalmohamed800@gmail.com>
2019-07-05 11:03:32 +0200

    modules

---
+++ Erp/ClientApp/app/manufacturing/modules/module.ts
@@ -0,0 +1,17 @@
+export interface Products {
+    productid: string;
+    productname: string;
+    BillofMatrialname: string;
+    costofmanufacturing: string;
+}
+
+
+export interface BillofMatrials {
+    BillofMatrialid: number;
+    name: number;
+    validFrom: string;
+    validUntil: string;
+    Quantity: number;
+    price: string;
+
+}
