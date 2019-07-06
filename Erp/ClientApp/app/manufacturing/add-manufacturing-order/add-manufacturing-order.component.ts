6bbd46cda68a0c57fa0e0b7c02cf31dbce2ca541
jemy <mohamedgamalmohamed800@gmail.com>
2019-07-06 04:32:33 +0200

    edit

--- Erp/ClientApp/app/manufacturing/add-manufacturing-order/add-manufacturing-order.component.ts
+++ Erp/ClientApp/app/manufacturing/add-manufacturing-order/add-manufacturing-order.component.ts
@@ -2,22 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { createOrder } from '../modules/module'
 import { ManufacturingOrderService } from '../services/manufacturing-order.service';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-add-manufacturing-order',
   templateUrl: './add-manufacturing-order.component.html',
   styles: []
 })
 export class AddManufacturingOrderComponent implements OnInit {
-  order: createOrder[];
+  // order: createOrder;
 
   constructor(private manufacturingOrderService: ManufacturingOrderService, private router: Router) { }
 
   ngOnInit() {
   }
 
-  orderProduct() {
-    this.manufacturingOrderService.saveProduct(this.order);
-    this.router.navigate(["manufacturing/manufacturingOrders"]);
+  orderProduct(order: NgForm) {
+    this.manufacturingOrderService.saveProduct(order.value);
+    //this.router.navigate(["manufacturing/manufacturingOrders"]);
+    console.log(order.value);
   }
 }
